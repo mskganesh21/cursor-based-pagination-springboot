7ae9b40038afbc27bb176f7254d9b2e6ff4e578b
mskganesh21 <mskganesh21@gmail.com>
2026-04-27 14:52:43 +0530

    added the initial code setup along with swagger

---
+++ src/main/java/com/ganeshmsk/cursorbasedpagination/CursorbasedpaginationApplication.java
@@ -0,0 +1,13 @@
+package com.ganeshmsk.cursorbasedpagination;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursorbasedpaginationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursorbasedpaginationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ganeshmsk/cursorbasedpagination/CursorbasedpaginationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ganeshmsk.cursorbasedpagination;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursorbasedpaginationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
